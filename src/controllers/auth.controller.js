3bcf0790f38350858775c1b61cf7b6fd1c5e8ab1
guedesclaudio <cgsarmentosilva@gmail.com>
2022-09-14 15:28:01 -0300

    created auth.controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,37 @@
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
-
 import db from "../database/db.js";
+import { StatusCodes } from "http-status-codes"
+
+async function createUser (req, res) {
+
+    const {
+        name, 
+        email, 
+        cpf, 
+        password, 
+        confirmPassword,
+        adress
+    } = req.body
+
+    const encryptedPassword = bcrypt.hashSync(password, 10)
+
+    try {
+        
+        await db.collection("users").insertOne({
+            name: name.trim(),
+            email: email.trim(),
+            password: encryptedPassword,
+            cpf: cpf.trim(),
+            adress: adress.trim()
+        })
+
+        res.sendStatus(StatusCodes.CREATED)
+
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR)
+    }
+}
+
+export { createUser }
