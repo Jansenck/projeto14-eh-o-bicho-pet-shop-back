55b110afb17c3fe56e74d6536cbf73a66f2159af
guedesclaudio <cgsarmentosilva@gmail.com>
2022-09-14 17:00:01 -0300

    finalized signin

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { validationSignUp } from "../middlewares/auth.middleware.js"
-import { createUser } from "../controllers/auth.controller.js";
+import { validationSignUp, validationSignIn } from "../middlewares/auth.middleware.js"
+import { createUser, checkUser } from "../controllers/auth.controller.js";
 
 const router = express.Router()
 
 router.post("/signup", validationSignUp, createUser)
+router.post("/signin", validationSignIn, checkUser)
 
 export default router
