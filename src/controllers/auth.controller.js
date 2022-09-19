05fc91579fcff37c716a9882899ac7fcab350166
guedesclaudio <cgsarmentosilva@gmail.com>
2022-09-19 11:54:28 -0300

    fix createUser

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,6 +5,15 @@ import { StatusCodes } from "http-status-codes";
 
 async function createUser(req, res) {
 
+  const {
+    name, 
+    email, 
+    cpf, 
+    password, 
+    confirmPassword,
+    address
+} = req.body
+
   const encryptedPassword = bcrypt.hashSync(password, 10);
 
   try {
