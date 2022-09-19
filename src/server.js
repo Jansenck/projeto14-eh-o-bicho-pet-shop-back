54fe36487d0971d63c10422793597581f2ccad57
Tissyane <tissyane@gmail.com>
2022-09-19 16:00:48 -0300

    Changing cors

--- src/server.js
+++ src/server.js
@@ -8,15 +8,15 @@ import favoriteRouter from "./routes/favorites.router.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
-// app.use(
-//   cors({
-//     origin: true,
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  })
+);
 
 app.options("*", cors());
 
